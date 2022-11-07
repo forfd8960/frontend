cd491b8491c36296bd05b341b659e6e4923e8153
forfd8960 <forfd8960@gmail.com>
2022-11-07 22:23:27 +0800

    intro to react - little js

---
+++ intro-react/proj2/let_and_const.js
@@ -0,0 +1,29 @@
+let name = "my name";
+console.log(name);
+
+name = "John";
+console.log(name);
+
+function printName(name) {
+    console.log(name);
+}
+
+printName('Bob');
+
+const printName1 = (name) => {
+    console.log(name);
+}
+
+printName1('Alex');
+
+const fn = () => {
+    console.log("arrow function with no arguments");
+}
+fn();
+
+const fnWithMiltiArgs = (name, age, weight) => {
+    console.log(name);
+    console.log(age);
+    console.log(weight);
+}
+fnWithMiltiArgs('Alice', 20, "50kg");
---
+++ javascript/week1/variable.js
@@ -0,0 +1,14 @@
+console.log("Hello, World");
+
+var person;
+console.log(person);
+
+person = "John";
+console.log(person);
+console.log("Hello", person);
+
+var greeting = "Hello";
+console.log(greeting, person);
+
+person = "Jane";
+console.log(greeting, person);
