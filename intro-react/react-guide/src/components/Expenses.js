5b103b1be6a7e26e2154c37bbb55ab4a6063ac52
forfd8960 <forfd8960@gmail.com>
2022-11-11 09:18:10 +0800

    React Guide - Reacfor to for array map

--- intro-react/react-guide/src/components/Expenses.js
+++ intro-react/react-guide/src/components/Expenses.js
@@ -3,25 +3,13 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
-    const items = props.items;
+  const items = props.items;
 
   return (
-    <div className='expenses'>
-      <ExpenseItem
-        title={items[0].title}
-        price={items[0].price}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        price={items[1].price}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        price={items[2].price}
-        date={items[2].date}
-      />
+    <div className="expenses">
+      {items.map((item, i) => (
+        <ExpenseItem title={item.title} price={item.price} date={item.date} key = {i}/>
+      ))}
     </div>
   );
 }
