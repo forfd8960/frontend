30cdb4f8f72497351bff01c49743d800bc4711a1
forfd8960 <forfd8960@gmail.com>
2022-11-13 00:36:44 +0800

    React Guide - React useState

--- intro-react/react-guide/src/components/Expense/ExpenseItem.js
+++ intro-react/react-guide/src/components/Expense/ExpenseItem.js
@@ -1,18 +1,23 @@
-import "./ExpenseItem.css";
+import React, {useState} from 'react';
 
 import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked!");
+    setTitle(title + " Updated!");
+    console.log(title + " Updated!");
   }
+
   return (
     <div className="expense-item">
       <ExpenseDate date = {props.date}/>
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
