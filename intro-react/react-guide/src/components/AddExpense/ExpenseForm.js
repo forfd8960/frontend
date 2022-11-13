caa2105d3132dc0ab94324f085a0c6809ab7bd25
forfd8960 <forfd8960@gmail.com>
2022-11-13 12:48:39 +0800

    React Guide - React Multi useState for Input Field

--- intro-react/react-guide/src/components/AddExpense/ExpenseForm.js
+++ intro-react/react-guide/src/components/AddExpense/ExpenseForm.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputAmount, setInputAmount] = useState("0.01");
+  const [inputDate, setInputDate] = useState("");
+
+  const titleChangeHdler = (event) => {
+    console.log("title changed: ", event.target.value);
+    setInputTitle(event.target.value);
+  };
+
+  const amountChangeHdler = (event) => {
+    console.log("amount changed: ", event.target.value);
+    setInputAmount(event.target.value);
+  };
+
+  const dateChangeHdler = (event) => {
+    console.log("date changed: ", event.target.value);
+    setInputDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="add-expense__controls">
         <div className="add-expense__control">
           <label>Title</label>
-          <input type='text' />
+          <input type="text" onChange={titleChangeHdler} />
         </div>
 
         <div className="add-expense__control">
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHdler}
+          />
         </div>
 
         <div className="add-expense__control">
           <label>Date</label>
-          <input type='date' min='202-11-01' max='2023-12-31'/>
+          <input
+            type="date"
+            min="202-11-01"
+            max="2023-12-31"
+            onChange={dateChangeHdler}
+          />
         </div>
       </div>
       <div className="add-expense__actions">
-        <button type='submit'>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
