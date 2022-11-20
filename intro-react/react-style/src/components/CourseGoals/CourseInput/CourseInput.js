8b5f1403508e3604b8fc0187577f1042a3e3919a
forfd8960 <forfd8960@gmail.com>
2022-11-20 15:43:46 +0800

    Styling React - How to Set Dynamic Style

--- intro-react/react-style/src/components/CourseGoals/CourseInput/CourseInput.js
+++ intro-react/react-style/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,11 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    const value = event.target.value;
+    if (value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
@@ -22,12 +27,11 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
-          style={{ borderColor: !isValid ? "red" : "black", background: !isValid ? "red" : "blue"}}
         />
       </div>
       <Button type="submit">Add Goal</Button>
